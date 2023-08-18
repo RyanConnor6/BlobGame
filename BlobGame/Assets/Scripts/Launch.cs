89c554e9cbc6dd9ceddae6ff50d6062610f01bad
Ryan Connor <40437041@live.napier.ac.uk>
2023-08-18 19:19:04 +0100

    Shooting With Arrow

---
+++ BlobGame/Assets/Scripts/Launch.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    public float launchForce = 10f;
+    public GameObject arrowPrefab; // Assign the 2D arrow sprite prefab in the Inspector
+
+    private Rigidbody2D rb;
+    private Vector2 dragStartWorldPosition;
+    private GameObject arrowInstance;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStartWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (arrowPrefab != null)
+            {
+                arrowInstance = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+            }
+        }
+
+        if (arrowInstance != null)
+        {
+            arrowInstance.transform.position = transform.position;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 currentMouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 launchDirection = (currentMouseWorldPosition - dragStartWorldPosition).normalized;
+
+            if (arrowInstance != null)
+            {
+                float angle = Mathf.Atan2(launchDirection.y, launchDirection.x) * Mathf.Rad2Deg;
+                arrowInstance.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (arrowInstance != null)
+            {
+                Destroy(arrowInstance);
+            }
+
+            Vector2 dragEndWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 launchDirection = (dragEndWorldPosition - dragStartWorldPosition).normalized;
+
+            rb.velocity = launchDirection * launchForce;
+        }
+    }
+}
