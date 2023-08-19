6f2ad6b1391248a22b44d85f33ce2826298237c2
Ryan Connor <40437041@live.napier.ac.uk>
2023-08-19 16:29:58 +0100

    GeneratedColours

--- BlobGame/Assets/Scripts/procgene.cs
+++ BlobGame/Assets/Scripts/procgene.cs
@@ -8,6 +8,13 @@ public class procgene : MonoBehaviour
     public Tilemap groundTilemap;
     public Tilemap obstacleTilemap;
     public Tile groundTile;
+    public Tile stage1Tile;
+    public Tile stage2Tile;
+    public Tile stage3Tile;
+    public Tile stage4Tile;
+    public Tile stage5Tile;
+    public Tile stage6Tile;
+    public Tile stage7Tile;
     public Tile pillarTile;
 
     public int mapWidth = 20;
@@ -42,14 +49,84 @@ public class procgene : MonoBehaviour
             }
 
             Vector3Int tilePosition = new Vector3Int(x, -2, 0);
-            groundTilemap.SetTile(tilePosition, groundTile);
+
+            if (x < 100)
+            {
+                groundTilemap.SetTile(tilePosition, groundTile);
+            }else if (x < 200)
+            {
+                groundTilemap.SetTile(tilePosition, stage1Tile);
+            }
+            else if (x < 300)
+            {
+                groundTilemap.SetTile(tilePosition, stage2Tile);
+            }
+            else if (x < 400)
+            {
+                groundTilemap.SetTile(tilePosition, stage3Tile);
+            }
+            else if (x < 500)
+            {
+                groundTilemap.SetTile(tilePosition, stage4Tile);
+            }
+            else if (x < 600)
+            {
+                groundTilemap.SetTile(tilePosition, stage5Tile);
+            }
+            else if (x < 700)
+            {
+                groundTilemap.SetTile(tilePosition, stage6Tile);
+            }
+            else if (x < 800)
+            {
+                groundTilemap.SetTile(tilePosition, stage7Tile);
+            }
+            else
+            {
+                groundTilemap.SetTile(tilePosition, groundTile);
+            }
 
             if (x == lastPillarx + pillarSpacing)
             {
                 pillarHeight = Random.Range(minpillarHeight, maxpillarHeight);
                 for (int h = 1; h < pillarHeight; h++)
                 {
-                    obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), pillarTile);
+                    if (x < 100)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), pillarTile);
+                    }
+                    else if (x < 200)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage1Tile);
+                    }
+                    else if (x < 300)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage2Tile);
+                    }
+                    else if (x < 400)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage3Tile);
+                    }
+                    else if (x < 500)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage4Tile);
+                    }
+                    else if (x < 600)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage5Tile);
+                    }
+                    else if (x < 700)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage6Tile);
+                    }
+                    else if (x < 800)
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), stage7Tile);
+                    }
+                    else
+                    {
+                        obstacleTilemap.SetTile(tilePosition + new Vector3Int(0, h, 0), pillarTile);
+                    }
                 }
                 newSpacing = true;
             }
