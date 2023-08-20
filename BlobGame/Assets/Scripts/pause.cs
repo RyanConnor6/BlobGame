34783c68afba0ae7f0cefd2e6ddc6937e5436627
Ryan Connor <40437041@live.napier.ac.uk>
2023-08-20 18:25:40 +0100

    Adding pause behaviour

--- BlobGame/Assets/Scripts/pause.cs
+++ BlobGame/Assets/Scripts/pause.cs
@@ -1,32 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pause : MonoBehaviour
 {
+    public GameObject pausePanel;
+    public GameObject uiPanel;
+    public Button resumeButton;
+    public Button quitButton;
+
     private bool isPaused = false;
+
+    private void Start()
+    {
+        // Set up button click listeners
+        resumeButton.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(Quit);
+        pausePanel.SetActive(false);
+        uiPanel.SetActive(true);
+    }
 
     private void Update()
     {
+        // Check for pause input (e.g., using the "Escape" key)
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            TogglePause();
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
         }
     }
 
-    private void TogglePause()
+    private void Pause()
     {
-        isPaused = !isPaused;
+        isPaused = true;
+        Time.timeScale = 0f; // Pause the game
+        pausePanel.SetActive(true);
+        uiPanel.SetActive(false);
+    }
 
-        if (isPaused)
-        {
-            Time.timeScale = 0f; // Pause the game by setting time scale to 0
-            Debug.Log("Game Paused");
-        }
-        else
-        {
-            Time.timeScale = 1f; // Resume the game by setting time scale to 1
-            Debug.Log("Game Resumed");
-        }
+    private void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; // Resume the game
+        pausePanel.SetActive(false);
+        uiPanel.SetActive(true);
+    }
+
+    private void Quit()
+    {
+        Application.Quit(); // Quit the game (this is a simple example)
     }
 }
